82ec7261faee89f89f47d2f710bf0d4135eac08c
Felipe <felipe.paulmartins@outlook.com>
2018-03-30 19:22:49 +0200

    Initial commit with inital files & config

---
+++ README.txt
@@ -0,0 +1,11 @@
+=== Jobtrek Blocks ===
+Contributors: Kuuak
+Tags: gutenberg, editor, block, admin
+Requires at least: 4.9
+Tested up to: 4.9.4
+Stable tag: 1.0.0
+Requires at least Gutenberg: 2.5
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Jobtrek block components for Gutenberg editor
---
+++ jobtrek-blocks.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Plugin Name: Jobtrek Blocks
+ * Plugin URI: https://github.com/Kuuak/jobtrek-blocks
+ * Description: Jobtrek block components for Gutenberg editor
+ * Author: Kuuak
+ * Author URI: https://github.com/Kuuak
+ * Version: 1.0.0
+ * License: GPL2+
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
+ *
+ * @package JOBTREK_BLOCKS
+ */
+
+//  Exit if accessed directly.
+defined('ABSPATH') || exit;
